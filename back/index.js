dce25ed099e87e0167328743be3f161bf2cac8b2
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-27 21:14:15 +0100

    add const server = express and configure the root to (/) in index.js

--- back/index.js
+++ back/index.js
@@ -1,2 +1,11 @@
 // Imports 
 const express= require('express');
+
+// Instantiate server 
+const server = express();
+
+// Configure routes
+server.get('/',function(req,res){
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200).send('<h1>test</h1>')
+});
