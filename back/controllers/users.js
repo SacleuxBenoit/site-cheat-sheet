edd226546354014233bc2b831394b46dc18de5e2
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-03-03 20:52:03 +0100

    add the route login and register in the module.exports of the controller users.js

--- back/controllers/users.js
+++ back/controllers/users.js
@@ -1,3 +1,14 @@
 // Imports
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken')
+
+// Routes
+
+module.exports = {
+    register: function(req,res){
+
+    },
+    login: function(req, res){
+
+    }
+}
