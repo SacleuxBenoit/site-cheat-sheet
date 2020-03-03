d10e2434efd4963cff1d676d2f66db417e9e9fdd
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-03-03 21:06:30 +0100

    add route for login and register in routes/index.js

--- back/routes/index.js
+++ back/routes/index.js
@@ -5,4 +5,10 @@ const usersController = require('../controllers/users');
 // Router
 exports.router = (function(){
     const router = express.Router();
-});
+
+    router.route('/users/register/').post(usersController.register);
+    router.route('/users/login/').post(usersController.login);
+
+    return router
+
+})();
