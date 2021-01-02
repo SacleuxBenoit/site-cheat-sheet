5d30d145d7209e8fc2c0f1a418699ecbc45f7023
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-01-02 20:36:16 +0100

    create the file front/createArticles.php with a form in it

---
+++ front/createArticles.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="#" method="POST">
+    <p>
+        <label for="articleTitle">Title : </label>
+        <input type="text" name="articleTitle" id="articleTitle">
+    </p>
+    <p>
+        <label for="articleContent">Content : </label>
+        <input type="text" name="articleContent" id="articleContent">
+    </p>
+    <p>
+        <input type="submit" value="Envoyez">
+    </p>
+    </form>
+</body>
+</html>
