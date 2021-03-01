e8263c1a32885e5ea49cc4b9874c4cc531e7ece9
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-01 14:26:12 +0100

    delete node_module && config.json :arrow_right: .gitignore, change the name of the database :arrow_right: connection_database.php

--- database/connection_database.php
+++ database/connection_database.php
@@ -1,7 +1,7 @@
 <?php 
 try
 {
-    $bdd = new PDO('mysql:host=localhost;dbname=comment_test;charset=utf8', $_SESSION['user'], $_SESSION['pass']);
+    $bdd = new PDO('mysql:host=localhost;dbname=cheatSheets;charset=utf8', $_SESSION['user'], $_SESSION['pass']);
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(Exception $e)
